efd6b796c618fedc3838b89f9196d289d50a95d8
danielpalli <dpallicolque@est.frba.utn.edu.ar>
2022-06-05 00:19:13 -0300

    Update

--- Angular/Componentes-Palli/karma.conf.js
+++ Angular/Componentes-Palli/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/proyecto_prueba'),
+      dir: require('path').join(__dirname, './coverage/componentes-palli'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Angular/Componentes-Palli/src/app/app.component.spec.ts
+++ Angular/Componentes-Palli/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'proyecto_prueba'`, () => {
+  it(`should have as title 'Componentes-Palli'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyecto_prueba');
+    expect(app.title).toEqual('Componentes-Palli');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyecto_prueba app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Componentes-Palli app is running!');
   });
 });
